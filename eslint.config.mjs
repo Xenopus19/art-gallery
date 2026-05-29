130a08d4d5a71732d9a85a074eeb657ecbe3c996
Xenopus19 <82256288+Xenopus19@users.noreply.github.com>
2026-05-29 14:25:43 +0300

    Workflow added

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -4,6 +4,12 @@ import tseslint from "typescript-eslint";
 export default [
   { files: ["**/*.ts"] },
 
+  {
+    ignores: [
+      "dist/*", 
+    ],
+  },
+
   pluginJs.configs.recommended,
 
   ...tseslint.configs.recommended,
